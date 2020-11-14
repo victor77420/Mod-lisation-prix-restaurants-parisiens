79d68be194d1255333c93be8c0d78a2f64064668
victor77420 <62885679+victor77420@users.noreply.github.com>
2020-11-14 16:47:11 +0100

    J'ai réussi à récupérer les liens

--- Scrap TripAdvisor.py
+++ Scrap TripAdvisor.py
@@ -11,11 +11,17 @@ Created on Thu Nov 12 19:17:41 2020
 import urllib
 from urllib import request
 import bs4
-import re
 
+#On prend l'URL de la page de recueil des restaurants Parisiens de TripAdvisor
 url = 'https://www.tripadvisor.fr/Restaurants-g187147-Paris_Ile_de_France.html'
 request_text = request.urlopen(url).read()
 page = bs4.BeautifulSoup(request_text, "lxml")
 
-data = page.find('script', text = re.compile('window.__WEB_CONTEXT__'))
-#On obtient un "script" avec plein d'infos sur plein de restaurants dont les liens TripAdvisor, les noms... mais je ne sais pas comment les extraire 
+#Les liens qui nous intéressent sont contenues dans les "divs" dont la classe est la suivante (parfois il faut ajouter un espace après "widget" pour que ça marche):
+data = page.find_all('div', {"class":"react-container component-widget"})
+
+#En visualisant le contenu de "data", on se rend compte que les liens sont contenus dans les "a" :
+ls = data[2].contents[0].find_all('a')
+
+#On récupère tous les liens :
+restaurants_index = {i:  {'url': 'https://www.tripadvisor.fr' + ls[i]['href']} for i in range(len(ls))}
