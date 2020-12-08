5f8d5d414c9157c2c541dbeac4687b2b8afd0804
victor77420 <62885679+victor77420@users.noreply.github.com>
2020-12-08 19:00:09 +0100

    Changement de fonctionnement
    Il survole chaque lien du data frame, et récupère les infos sur ce lien

--- Scrap info resto.py
+++ Scrap info resto.py
@@ -11,38 +11,42 @@ Created on Wed Nov 11 20:14:33 2020
 import urllib
 from urllib import request
 import bs4
+import pandas as pd
 
-#On prend l'URL d'un restaurant sur tripadvisor
+i = 0
+parisiens['Style de nourriture'] = 'Non renseigné'
+parisiens['Note Globale'] = "Non renseigné"
+parisiens['Catégorie de prix'] = "Non renseigné
+parisiens['Fourchette de prix'] = "Non renseigné"
+parisiens['Note de cuisine'] = "Non renseigné"
+parisiens['Note de service'] = "Non renseigné"
+parisiens['Note qualité-prix'] = "Non renseigné"
+parisiens['Note ambiance'] = "Non renseigné"
 
-url = "https://www.tripadvisor.fr/Restaurant_Review-g187147-d6575305-Reviews-Il_Etait_Un_Square-Paris_Ile_de_France.html"
-request_text = request.urlopen(url).read()
-page = bs4.BeautifulSoup(request_text, "lxml")
+#Pour chaque lien, on va récupérer les informations du restaurant associé
+for lien in parisiens['Lien TripAdvisor']:
+  url = parisiens['Lien TripAdvisor'][i]
+  request_text = request.urlopen(url).read()
+  page = bs4.BeautifulSoup(request_text, "lxml")
 
-#On extrait les informations sur ce restaurant
+  #Si ça ne marche pas, retirer l'espace après "'page'"
+  if page.find_all('div',{'class':'page '}) != []:
+    divs = page.find('div',{'class':'page '})
+    infos = divs.find_all('a')
+    parisiens['Style de nourriture'][i] = infos[6].contents
+    parisiens['Note Globale'][i] = infos[1].contents[0]['title'][0:3]
+    parisiens['Catégorie de prix'][i] = infos[3].contents
 
-nom = page.find_all('div',{"id":"taplc_top_info_0"})[0].find_all('h1')[0].contents
+  if page.find_all('div',{"class":"_1XLfiSsv"}) != []:
+    infos2 = page.find_all('div',{"class":"_1XLfiSsv"})
+    parisiens['Fourchette de prix'][i] = infos2[0].contents[0]
 
-#Si ça ne marche pas, retirer l'espace après "'page'"
-divs = page.find('div',{'class':'page '})
-infos = divs.find_all('a')
-adresse = infos[7].contents
-service = infos[5].contents
-style = infos[6].contents
-note = infos[1].contents[0]['title'][0:3]
-prix = infos[3].contents
-
-if page.find_all('div',{"class":"_1XLfiSsv"}) == []:
-  infos2 = page.find_all('div',{"class":"_1XLfiSsv"})
-  fourchette_prix = infos2[0].contents[0]
-else:
-  fourchette_prix = NaN
-
-infos3 = page.find_all('span',{"class":"_377onWB-"})
-note_cuisine = int(infos3[0].contents[0]['class'][1][7:])/10
-note_service = int(infos3[1].contents[0]['class'][1][7:])/10
-note_QP = int(infos3[2].contents[0]['class'][1][7:])/10
-#Boucle "if" car pas tous les restaurants ont la note d'ambiance
-if len(infos3) == 4:
-  note_ambiance = int(infos3[3].contents[0]['class'][1][7:])/10
-else:
-  note_ambiance = NaN
+  if page.find_all('span',{"class":"_377onWB-"}) != []:
+    infos3 = page.find_all('span',{"class":"_377onWB-"})
+    parisiens['Note de cuisine'][i] = int(infos3[0].contents[0]['class'][1][7:])/10
+    parisiens['Note de service'][i] = int(infos3[1].contents[0]['class'][1][7:])/10
+    parisiens['Note qualité-prix'][i] = int(infos3[2].contents[0]['class'][1][7:])/10
+  #Boucle "if" car pas tous les restaurants ont la note d'ambiance
+    if len(infos3) == 4:
+      parisiens['Note ambiance'][i] = int(infos3[3].contents[0]['class'][1][7:])/10 
+  i = i + 1
