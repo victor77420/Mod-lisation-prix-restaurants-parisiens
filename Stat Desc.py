a99eff01761f05e27f0ae1b1b2a7a1dc0fc31c2d
victor77420 <62885679+victor77420@users.noreply.github.com>
2020-12-12 14:43:46 +0100

    J'ai rendu le code plus propre

--- Stat Desc.py
+++ Stat Desc.py
@@ -9,91 +9,120 @@ Created on Fri Dec 11 22:48:21 2020
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+parisiens = pd.read_csv('/Users/victorhuynh/Documents/ENSAE/ENSAE 2A/2A S1/PDS/Projet/victoire.csv', sep = ";")
 
-parisiens_test2 = pd.read_excel('parisiens_test2_avec_note.xlsx')
-
-parisiens_test3 = parisiens_test2.copy()
-parisiens_test3 = parisiens_test3.drop('Style de nourriture', axis=1)
-parisiens_test3 = parisiens_test3.drop('index', axis=1)
-parisiens_test3 = parisiens_test3.drop('Unnamed: 0', axis=1)
-parisiens_test3 = parisiens_test3.drop('level_0', axis=1)
-parisiens_test3 = parisiens_test3.drop('Unnamed: 0.1', axis=1)
-parisiens_test3 = parisiens_test3.astype({'Catégorie de prix':'str'})
-parisiens_test3 = parisiens_test3[parisiens_test3['Catégorie de prix'].str.contains('€')].reset_index(drop = True)
-
-df = parisiens_test3.groupby('Catégorie de prix').aggregate({'name':'count'}).reset_index()
-df = df.rename(columns={'name': 'Effectif'})
-sns.catplot(x='Catégorie de prix', y='Effectif', edgecolor="black", data=df,kind = "bar", color = "cyan")
+df = parisiens.groupby('Catégorie de prix').aggregate({'name':'count'}).reset_index()
+df = df.rename(columns={'name': 'Nombre de restaurants'})
+sns.catplot(x='Catégorie de prix', y='Nombre de restaurants', edgecolor="black", data=df,kind = "bar", color = "cyan")
 
 ##Quelques tendances
         
 #Moyenne de note globale par catégorie de prix
+parisiens = parisiens[parisiens['Note Globale'] != 'NR']
+parisiens = parisiens[parisiens['Note Globale'] != 'Non renseigné']
 
-parisiens_test3['Note Globale'] = parisiens_test3['Note Globale'].str.replace(",", ".").astype(float)
-parisiens_test3.groupby('Catégorie de prix').aggregate({'Note Globale' : 'mean'})
+parisiens['Note Globale'] = parisiens['Note Globale'].str.replace(",", ".").astype(float)
+parisiens.groupby('Catégorie de prix').aggregate({'Note Globale' : 'mean'})
 
 #Moyenne du nombre d'avis par catégorie de prix
 
-parisiens_test3['Nombre avis'] = parisiens_test3['Nombre avis'].astype(int)
-df = parisiens_test3.groupby('Catégorie de prix').aggregate({'Nombre avis' : 'mean'}).reset_index()
+parisiens['Nombre avis'] = parisiens['Nombre avis'].astype(int)
+df = parisiens.groupby('Catégorie de prix').aggregate({'Nombre avis' : 'mean'}).reset_index()
 df = df.rename(columns={'Nombre avis': "Nombre moyen d'avis"})
 sns.catplot(x='Catégorie de prix', y="Nombre moyen d'avis", edgecolor="black", data=df,kind = "bar", color = "yellow")
 
 #Moyenne de note de cuisine par catégorie de prix
 
-parisiens_avec_note_cuisine = parisiens_test3[parisiens_test3['Note de cuisine'] != 'Non renseigné']
+parisiens_avec_note_cuisine = parisiens[parisiens['Note de cuisine'] != 'Non renseigné']
 parisiens_avec_note_cuisine = parisiens_avec_note_cuisine.astype({'Note de cuisine' : 'float'})
 parisiens_avec_note_cuisine.groupby('Catégorie de prix').aggregate({'Note de cuisine' : 'mean'})
 
 #Moyenne de note de service par catégorie de prix
 
-parisiens_avec_note_service = parisiens_test3[parisiens_test3['Note de service'] != 'Non renseigné']
+parisiens_avec_note_service = parisiens[parisiens['Note de service'] != 'Non renseigné']
 parisiens_avec_note_service = parisiens_avec_note_service.astype({'Note de service' : 'float'})
 parisiens_avec_note_service.groupby('Catégorie de prix').aggregate({'Note de service' : 'mean'})
 
 #Moyenne de note QP par catégorie de prix
 
-parisiens_avec_note_QP = parisiens_test3[parisiens_test3['Note qualité-prix'] != 'Non renseigné']
+parisiens_avec_note_QP = parisiens[parisiens['Note qualité-prix'] != 'Non renseigné']
 parisiens_avec_note_QP = parisiens_avec_note_QP.astype({'Note qualité-prix' : 'float'})
 parisiens_avec_note_QP.groupby('Catégorie de prix').aggregate({'Note qualité-prix' : 'mean'})
 
 #Moyenne de note d'ambiance par catégorie de prix
 
-parisiens_avec_note_ambiance = parisiens_test3[parisiens_test3['Note ambiance'] != 'Non renseigné']
+parisiens_avec_note_ambiance = parisiens[parisiens['Note ambiance'] != 'Non renseigné']
 parisiens_avec_note_ambiance = parisiens_avec_note_ambiance.astype({'Note ambiance' : 'float'})
 parisiens_avec_note_ambiance.groupby('Catégorie de prix').aggregate({'Note ambiance' : 'mean'})
 
 ##On va prendre une catégorie de prix par une, et représenter le pourcentage de restaurants par arrondissement dans cette catégorie de prix
 
-parisiens_cheap = parisiens_test3[parisiens_test3['Catégorie de prix'] == "['€']"].reset_index(drop = True)
-parisiens_average = parisiens_test3[parisiens_test3['Catégorie de prix'] == "['€€-€€€']"].reset_index(drop = True)
-parisiens_expensive = parisiens_test3[parisiens_test3['Catégorie de prix'] == "['€€€€']"].reset_index(drop = True)
+parisiens_cheap = parisiens[parisiens['Catégorie de prix'] == 'faible'].reset_index(drop = True)
+parisiens_average = parisiens[parisiens['Catégorie de prix'] == 'moyen'].reset_index(drop = True)
+parisiens_expensive = parisiens[parisiens['Catégorie de prix'] == 'élevé'].reset_index(drop = True)
 
+#On compte le nombre de restaurants par arrondissement
 from matplotlib.ticker import MultipleLocator
-effectif_total_par_arr = parisiens_test3.groupby('arr').aggregate({'arr' : 'count'})['arr']
+effectif_total_par_arr = parisiens.groupby('arr').aggregate({'arr' : 'count'})['arr']
 
+#On compte le nombre de restaurants "cheap" par arrondissement et on trace leur fréquence par arrondissement
 effectif_cheap = parisiens_cheap.groupby('arr').aggregate({'arr' : 'count'})['arr']
 arrondissements_cheap = range(1,21)
-
 ax = plt.axes()
 ax.xaxis.set_major_locator(MultipleLocator(1))
 plt.bar(arrondissements_cheap,effectif_cheap/effectif_total_par_arr, color = "#ABEBC6", edgecolor="black",linewidth=1, ecolor = "green",capsize = 10)
-        
+
+#On compte le nombre de restaurants "average" par arrondissement et on trace leur fréquence par arrondissement
 effectif_average = parisiens_average.groupby('arr').aggregate({'arr' : 'count'})['arr']
 arrondissements_average = range(1,21)
-
 ax = plt.axes()
 ax.xaxis.set_major_locator(MultipleLocator(1))
 plt.bar(arrondissements_average,effectif_average/effectif_total_par_arr, color = "#F0B27A", edgecolor="black",linewidth=1, ecolor = "green",capsize = 10)
-        
+
+#On compte le nombre de restaurants "expensive" par arrondissement et on trace leur fréquence par arrondissement
 effectif_expensive = parisiens_expensive.groupby('arr').aggregate({'arr' : 'count'})['arr']
 arrondissements_expensive = range(1,21)
-
 ax = plt.axes()
 ax.xaxis.set_major_locator(MultipleLocator(1))
 plt.bar(arrondissements_expensive,effectif_expensive/effectif_total_par_arr, color = "#EC7063", edgecolor="black",linewidth=1, ecolor = "green",capsize = 10)
         
-##Etude de la variable "Fourchette de prix" pour avoir les idées plus claires avec des prix en chiffres
+##Etude de la variable "Prix moyen" pour avoir les idées plus claires avec des prix en chiffres
+
+#Etudions le prix moyen d'un restaurant par arrondissement
+
+df = parisiens.groupby('arr').aggregate({'Prix moyen' : 'mean'}).reset_index()
+df = df.rename(columns = {'arr' : 'Arrondissement'})
+sns.catplot(x='Arrondissement', y='Prix moyen', edgecolor="black", data=df, kind = "bar", color = "pink")
+
+#Traçons l'évolution du prix d'un restaurant en fonction de l'année de sa créaton
+
+df = parisiens.groupby('foundingYear').aggregate({'Prix moyen' : 'mean'}).reset_index()
+df = df.rename(columns = {'foundingYear' : 'Année de création'})
+sns.lineplot(x='Année de création', y='Prix moyen', data=df, color = "green")
+
+#Voyons le prix d'un restaurant selon le style de nourriture
+
+df = parisiens.groupby('Style de nourriture').aggregate({'Prix moyen' : 'mean'}).reset_index()
+g = sns.catplot(x='Style de nourriture', y='Prix moyen', edgecolor="black", data=df, kind = "bar", color = "orange")
+g.set_xticklabels(rotation=90) #Pour ne pas que les intitulés se chevauchent
+
+#Comparons taille d'entreprise d'un restaurant et prix moyen du repas dans ce restaurant
+
+df = parisiens.groupby('categorySize').aggregate({'Prix moyen' : 'mean'}).reset_index()
+df = df.rename(columns = {'categorySize' : 'Taille du restaurant'})
+sns.catplot(x='Taille du restaurant', y='Prix moyen', edgecolor="black", data=df, kind = "bar", color = "blue")
+
+
+
+
+
+
+
+
+
+
+
+## INUTILE On l'a replacé autre part : le bout de code pour obtenir la variable 'Prix moyen'
 
 parisiens_test4 = parisiens_test3[parisiens_test3['Fourchette de prix'].str.contains('€')].reset_index(drop=True)
 parisiens_test4['Fourchette prix inf'] = 'Non renseigné'
@@ -121,27 +150,3 @@ parisiens_test4 = parisiens_test4[parisiens_test4['Fourchette prix sup'] != '23\
 
 parisiens_test4 = parisiens_test4.astype({'Fourchette prix inf' : 'int64','Fourchette prix sup' : 'int64'})
 parisiens_test4['Prix moyen'] = (parisiens_test4['Fourchette prix inf'] + parisiens_test4['Fourchette prix sup']) / 2
-
-#Etudions le prix moyen d'un restaurant par arrondissement
-
-df = parisiens_test4.groupby('arr').aggregate({'Prix moyen' : 'mean'}).reset_index()
-df = df.rename(columns = {'arr' : 'Arrondissement'})
-sns.catplot(x='Arrondissement', y='Prix moyen', edgecolor="black", data=df, kind = "bar", color = "pink")
-
-#Traçons l'évolution du prix d'un restaurant en fonction de l'année de sa créaton
-
-df = parisiens_test4.groupby('foundingYear').aggregate({'Prix moyen' : 'mean'}).reset_index()
-df = df.rename(columns = {'foundingYear' : 'Année de création'})
-sns.lineplot(x='Année de création', y='Prix moyen', data=df, color = "green")
-
-#Voyons le prix d'un restaurant selon le type de service
-
-df = parisiens_test4.groupby('categoryLabel').aggregate({'Prix moyen' : 'mean'}).reset_index()
-df = df.rename(columns = {'categoryLabel' : 'Type de service'})
-sns.catplot(x='categoryLabel', y='Prix moyen', edgecolor="black", data=df, kind = "bar", color = "orange")
-
-#Comparons taille d'entreprise d'un restaurant et prix moyen du repas dans ce restaurant
-
-df = parisiens_test4.groupby('categorySize').aggregate({'Prix moyen' : 'mean'}).reset_index()
-#df = df.rename(columns = {'arr' : 'Arrondissement'})
-sns.catplot(x='categorySize', y='Prix moyen', edgecolor="black", data=df, kind = "bar", color = "blue")
